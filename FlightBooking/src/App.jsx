9f947585e369decd82f536b3cf6a32f651483d8f
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-08-01 23:47:25 +0200

    more

--- FlightBooking/src/App.jsx
+++ FlightBooking/src/App.jsx
@@ -1,34 +1,32 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./component/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./component/Footer";
+import { Routes, Route } from "react-router-dom";
 
 import BookFlight from "./pages/BookFlight";
-import Footer from "./component/Footer";
-import Dashboard from "./dashboard/Dashboard";
+import MyBooking from "./pages/MyBooking";
 import YourFlights from "./pages/YourFlights";
+import Dashboard from "./dashboard/Dashboard";
 import AllFlights from "./dashboard/AllFlights";
 import AvailableFlights from "./dashboard/AvailableFlights";
 import CreateFlight from "./dashboard/CreateFlight";
 import BookedFlights from "./dashboard/BookedFlights";
 import ParentPage from "./dashboard/ParentPage";
-import MyBooking from "./pages/MyBooking";
 import FlightSearch from "./dashboard/FlightSearch";
 import FilterByPrice from "./dashboard/FilterByPrice";
-import { ParentContext } from "./context/ParentContext";
+
+const basename = import.meta.env.PROD ? "/FlightBooking-react" : "/";
 
 function App() {
-  const basename = import.meta.env.DEV ? "/" : "/FlightBooking-react";
   return (
     <BrowserRouter basename={basename}>
-    
       <Navbar />
       <Routes>
         <Route path="/BookFlight" element={<BookFlight />} />
         <Route path="MyBooking" element={<MyBooking />} />
         <Route path="YourFlights" element={<YourFlights />} />
 
-        {/* Nested routes under ParentPage */}
         <Route path="/" element={<ParentPage />}>
           <Route index element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />} />
--- FlightBooking/src/DefineConfig.jsx
+++ FlightBooking/src/DefineConfig.jsx
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FlightBooking-react/', // must match your repo name
+  base: '/FlightBooking-react/', // important!
 });
-
