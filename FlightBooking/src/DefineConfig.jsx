2a436cadfc80bb35c331ed1c2bd96df905ce8965
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-08-02 00:25:39 +0200

    d

--- FlightBooking/src/DefineConfig.jsx
+++ FlightBooking/src/DefineConfig.jsx
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FlightBooking-react/',  // important!
+  base: '/FlightBooking-react/', // << this is critical!
 });
