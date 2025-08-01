f2ce5c714ed9c29eec8e38216e36237bd8c41757
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-08-02 00:53:22 +0200

    updtenew

--- FlightBooking/src/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/FlightBooking-react/', // 👈 this must match your GitHub repo name
-})
