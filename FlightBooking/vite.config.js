0bf0f303f197279f5659643f09356f6ca9a90061
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-08-02 01:01:39 +0200

    new

--- FlightBooking/vite.config.js
+++ FlightBooking/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FlightBooking-react/', // 👈 this must match your GitHub repo name
-})
+  base: '/FlightBooking-react/', // 👈 MUST match your GitHub repo name
+});
